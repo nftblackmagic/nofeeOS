03049c05defec1e66e2f54c82e93404c595fd7fe
anzhang <zhangan@anzhangs-MacBook-Pro.local>
2022-07-08 02:19:54 -0700

    update

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = Object.freeze({
-    tokenId: '529',
+    tokenId: '3332',
     tokenAddress: '0xe0901883d567f50bdc0f090ebe7d0bb6d1dfe61f',
     accountAddress: '0x15DBcD4756aD188456a56a1fa3fD90147881fF75',
     startAmount: 0.001
--- sample.js
+++ sample.js
@@ -1,3 +1,5 @@
 module.exports = Object.freeze(
-
+    {
+        "parameters": ""
+    }
 )
